1771103db9326227f1b9db94a1d0dc96e594e182
Matees <mateespublicprofile@gmail.com>
2022-12-22 00:23:10 -0500

    conf

---
+++ after/plugin/lsp.lua
@@ -0,0 +1,20 @@
+local lsp = require("lsp-zero")
+
+lsp.preset('recommended')
+
+lsp.ensure_installed({
+  'tsserver',
+  'eslint',
+  'sumneko_lua',
+  'rust_analyzer',
+})
+
+local cmp = require('cmp')
+local cmp_select = {behavior = cmp.SelectBehavior.Select}
+local cmp_mappings = lsp.defaults.cmp_mappings({
+  ["<C-Space>"] = cmp.mapping.complete(),
+  ['CR'] = cmp.mapping.confirm({select = true}),
+})
+
+lsp.setup_nvim_cmp({mapping = cmp_mappings})
+lsp.setup()
---
+++ lua/matees/init.lua
@@ -0,0 +1,4 @@
+require("matees.set")
+require("matees.remap")
+
+vim.cmd[[colorscheme dracula]]
